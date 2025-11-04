63220d91975ee6d283679431a7a5690b2f8ed297
Ed <108343328+eded001@users.noreply.github.com>
2025-11-04 13:58:37 -0300

    feat: criei um componente para servir de main

---
+++ src/components/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main({ children }) {
+    return (
+        <main className="bg-blue-400 h-screen flex items-center justify-start p-5">
+            {children}
+        </main>
+    );
+}
