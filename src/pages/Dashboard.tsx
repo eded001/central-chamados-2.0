4624d94d4bb9bb24d25736e6f124f56dc9a053e1
Ed <108343328+eded001@users.noreply.github.com>
2025-12-16 13:55:59 -0300

    feat: add pages

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/Header";
+
+export default function Dashboard() {
+    return (
+        <section>
+            <h1 className="text-xl">Visão Geral</h1>
+            <Header />
+        </section>
+    )
+}
