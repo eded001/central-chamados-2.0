8d6b2fdd96af76269e116d078c176daff28dc028
Ed <108343328+eded001@users.noreply.github.com>
2025-12-16 19:22:28 -0300

    feat: add aspect ratio

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,3 +1,5 @@
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+
 interface LogoProps {
     color?: string;
     size?: number;
@@ -7,14 +9,20 @@ export default function Logo({ color = "bg-zinc-800", size = 50 }: LogoProps) {
     const image = "https://raw.githubusercontent.com/semcult-belem/central-chamados/refs/heads/main/frontend/public/assets/images/logo-prefeitura.png";
 
     return (
-        <div
-            className={`
-            w-${size} h-${size} ${color}
-            mask-[url('${image}')]
-            mask-center mask-no-repeat mask-contain
-            -webkit-mask-[url('${image}')]
-            -webkit-mask-center -webkit-mask-no-repeat -webkit-mask-contain
-        `}
-        />
+        <div style={{ width: size }}>
+            <AspectRatio ratio={1}>
+                <div
+                    className={`
+                        w-full h-full ${color}
+                        mask-[url('${image}')]
+                        mask-center mask-no-repeat mask-contain
+                        [-webkit-mask:url('${image}')]
+                        [-webkit-mask-position:center]
+                        [-webkit-mask-repeat:no-repeat]
+                        [-webkit-mask-size:contain]
+                    `}
+                />
+            </AspectRatio>
+        </div>
     );
 }
