c041b75ed3fc0b1372973a2ee666d08f629533e9
Ed <108343328+eded001@users.noreply.github.com>
2025-12-17 12:48:21 -0300

    feat: add routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,22 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
-import App from "./App";
 import Layout from "@/app/layout";
 import Dashboard from "@/pages/Dashboard";
 import Requests from "@/pages/Requests";
 import Settings from "@/pages/Settings";
 import Profile from "@/pages/Profile";
 import Tests from "@/pages/Tests";
+import About from "@/pages/About";
+import Account from "@/pages/Account";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="about" element={<About />} />
+        <Route path="account" element={<Account />} />
+
         <Route element={<Layout />}>
           <Route index element={<Dashboard />} />
           <Route path="requests" element={<Requests />} />
