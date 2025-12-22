abe9d22475997e3867061322b19ed15f2a3b87bc
Ed <108343328+eded001@users.noreply.github.com>
2025-12-22 11:14:49 -0300

    feat: using the CreateTicketDialog component

--- src/components/DashboardView.tsx
+++ src/components/DashboardView.tsx
@@ -2,22 +2,63 @@ import { Clock, MessageSquare, CheckCircle, ChevronRight } from "lucide-react";
 import { NavbarSearch } from "@/components/ui/navbar-seach";
 import KPICard from "@/components/KPICard";
 import TicketsTable from "@/components/TicketsTable";
+import { CreateTicketDialog } from "@/components/CreateTicketDialog";
 
 import type { Ticket } from "@/types/ticket.type";
 import type { KPIs } from "@/types/kpi.type";
 
-export default function DashboardView({ tickets, kpis }: { tickets: Ticket[]; kpis: KPIs }) {
+export default function DashboardView({
+    tickets,
+    kpis,
+}: {
+    tickets: Ticket[];
+    kpis: KPIs;
+}) {
+    function handleCreateTicket(data: {
+        title: string;
+        problem: string;
+        priority: string;
+        description?: string;
+    }) {
+        alert(
+            `Chamado criado:
+Título: ${data.title}
+Problema: ${data.problem}
+Prioridade: ${data.priority}
+Descrição: ${data.description ?? "N/A"}`
+        );
+    }
+
     return (
         <div className="space-y-8">
-            <NavbarSearch
-                searchPlaceholder="Pesquisar chamados"
-                className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-sky-500"
-            />
+            <div className="flex border rounded-md justify-between items-center gap-5 p-2">
+                <NavbarSearch
+                    searchPlaceholder="Pesquisar chamados"
+                    className="flex-1 focus:outline-none focus:ring-2 focus:ring-sky-500"
+                />
+
+                <CreateTicketDialog onCreate={handleCreateTicket} />
+            </div>
 
             <div className="grid gap-4 md:grid-cols-3">
-                <KPICard title="Em Aberto" value={kpis.open} icon={Clock} iconColor="text-blue-500" />
-                <KPICard title="Respondidos" value={kpis.replied} icon={MessageSquare} iconColor="text-yellow-500" />
-                <KPICard title="Resolvidos no Mês" value={kpis.resolvedMonth} icon={CheckCircle} iconColor="text-green-500" />
+                <KPICard
+                    title="Em Aberto"
+                    value={kpis.open}
+                    icon={Clock}
+                    iconColor="text-sky-500"
+                />
+                <KPICard
+                    title="Respondidos"
+                    value={kpis.replied}
+                    icon={MessageSquare}
+                    iconColor="text-yellow-500"
+                />
+                <KPICard
+                    title="Resolvidos no Mês"
+                    value={kpis.resolvedMonth}
+                    icon={CheckCircle}
+                    iconColor="text-green-500"
+                />
             </div>
 
             <div className="rounded-lg border">
